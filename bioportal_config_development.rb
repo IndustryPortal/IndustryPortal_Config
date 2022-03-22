d71392d97b8635e3576ae8d61d1a88bedccb1486
Abdel Wadoud <88406384+rasmi-aw@users.noreply.github.com>
2022-03-22 07:50:23 -0400

    Update bioportal_config_development.rb

--- bioportal_config_development.rb
+++ bioportal_config_development.rb
@@ -148,8 +148,9 @@ $ORG_SITE = ($ORG.nil? || $ORG.empty?) ? $SITE : "#{$ORG} #{$SITE}"
 #ExceptionNotifier.exception_recipients = [$ERROR_EMAIL]
 
 #Fairness including config
-$FAIRNESS_DISABLED = true
-$FAIRNESS_URL = ""
+$FAIRNESS_DISABLED = false
+$FAIRNESS_URL = "http://localhost:8080/fairness-assessment/?portal=agroportal"
+$HOSTNAME = "industryportal.enit.fr"
 
 #Coloaboration and support
 $HOME_PAGE_LOGOS = [
